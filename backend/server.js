6eb84f57e0df3d9f36c36f6d074e24f785958b35
Sailiid <neymarsaime@gmail.com>
2026-05-31 12:14:05 -0500

    configuracion con git y dependencias

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,47 @@
-import express from "express";
-import mysql from "mysql2";
-import cors from "cors";
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "tecnohogar"
+    host: "127.0.0.1",
+    user: "root",
+    password: "Lianestefania15*",
+    database: "contactos_formulario"
 });
 
-app.get("/productos", (req, res) => {
-  db.query("SELECT * FROM productos", (err, result) => {
-    if (err) return res.status(500).send(err);
-    res.json(result);
-  });
+db.connect((err) => {
+    if (err) {
+        console.error("Error de conexión:", err);
+    } else {
+        console.log("Conectado a MySQL");
+    }
+});
+app.get("/", (req, res) => {
+    res.send("Servidor conectado a MySQL");
 });
 
 app.post("/guardar", (req, res) => {
   const { nombre, correo, mensaje , telefono} = req.body;
-  db.query("INSERT INTO contactos (nombre, correo, mensaje, telefono) VALUES (?, ?, ?, ?)", [nombre, correo, mensaje, telefono], (err) => {
-    if (err) return res.status(500).send(err);
+  console.log("Datos recibidos:", req.body);
+  if (!nombre || !correo || !mensaje || !telefono) {
+    return res.status(400).send("Faltan datos requeridos");
+  }
+  const sql = "INSERT INTO contactos (nombre, correo, mensaje, telefono) VALUES (?, ?, ?, ?)";
+  db.query(sql, [nombre, correo, mensaje, telefono], (err, result) => {
+    if (err) {
+      console.error("Error SQL", err);
+      return res.status(500).send("Error en servidor");
+    }
+    console.log("Registro ingresado", result)
     res.send("Datos guardados correctamente");
   });
 });
 
-app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000")
+});
